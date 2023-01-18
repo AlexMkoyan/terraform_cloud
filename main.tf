084bfc78f0d45200fdc97a0346c374cc5c53a168
AlexMkoyan <you@example.com>
2023-01-18 23:44:11 +0400

    jlkjk

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket = "lennagan"
-    key    = "terraform/emportant"
-    region = "us-east-1"
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -15,11 +10,6 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-# resource "aws_s3_bucket" "example" {
-#   bucket = "lennagan"
-#   acl    = "private"
-# }
 
 resource "aws_instance" "myubuntu" {
   ami = "ami-0b5eea76982371e91"
